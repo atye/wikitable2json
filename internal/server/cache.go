b1a7388c55a5757b5bb4ad73d931ea3441b817e7
Aaron Tye <tyeaaron@gmail.com>
2024-12-10 09:49:54 -0500

    remove lock (#120)
    Co-authored-by: atye <aaron.tye@gmail.com>

--- internal/server/cache.go
+++ internal/server/cache.go
@@ -2,7 +2,6 @@ package server
 
 import (
 	"log"
-	"sync"
 	"time"
 
 	"github.com/hashicorp/golang-lru/v2/expirable"
@@ -10,7 +9,6 @@ import (
 
 type Cache struct {
 	lru *expirable.LRU[string, any]
-	mu  sync.RWMutex
 }
 
 func NewCache(size int, expiration time.Duration) *Cache {
@@ -20,14 +18,10 @@ func NewCache(size int, expiration time.Duration) *Cache {
 }
 
 func (c *Cache) Get(key string) (any, bool) {
-	c.mu.RLock()
-	defer c.mu.RUnlock()
 	return c.lru.Get(key)
 }
 
 func (c *Cache) Add(key string, value any) bool {
-	c.mu.Lock()
-	defer c.mu.Unlock()
 	log.Printf("cache: added %v\n", key)
 	return c.lru.Add(key, value)
 }
